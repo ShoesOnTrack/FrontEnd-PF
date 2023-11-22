47d5e7cc135aba36352e1f09fd36196876f3510a
Turco777 <agusalmona@gmail.com>
2023-11-22 12:32:15 -0300

    commit de action-types

---
+++ src/redux/action-type.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+import {
+  GET_ALL_PRODUCTS,
+  GET_ALL_CATEGORIES,
+  SEARCH_PRODUCTS,
+  GET_CART,
+  ADD_TO_CART,
+  UPDATE_CART,
+  REMOVE_FROM_CART,
+  SORT_PRICE,
+  GET_FAVORITES,
+} from "./action-type";
---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+export const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS';
+export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES';
+export const SEARCH_PRODUCTS = 'SEARCH_PRODUCTS';
+export const SORT_PRICE = 'SORT_PRICE';
+export const USER = 'USER';
+export const GET_CART = 'GET_CART';
+export const UPDATE_CART = 'UPDATE_CART';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const GET_FAVORITES = 'GET_FAVORITES';
