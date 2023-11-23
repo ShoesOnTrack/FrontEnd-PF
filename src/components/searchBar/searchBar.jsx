7033e1877d777978dc64c6d3381b8ea1903357d7
Jessuss <jesusacosta3321@gmail.com>
2023-11-23 13:56:56 -0300

    Funcionalidad SearchBar + algunos PNGs

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,7 +1,37 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce'
+import lupa from "@/helpers/assets/Lupa.png"
+import Image from 'next/image';
+
 const SearchBar = () => {
+    const [name, setName] = useState("");
+
+    const handleChange = (event) => {
+      setName(event.target.value);
+      handleSearch()
+    };
+  
+    const handleSearch = useDebouncedCallback(() => {
+      console.log(name);
+    }, 300);
+
     return (
         <div>
-            <h2>Soy la SEARCHBAR</h2>
+            
+      <input
+        type="search"
+        onChange={handleChange}
+        value={name}
+        placeholder="Search for a product..."
+      />
+      <button onClick={()=> setName("")}>
+      <Image
+       src={lupa}
+       width={15}
+       height={15}
+        alt="Search" />
+      </button>
         </div>
     );
   };
