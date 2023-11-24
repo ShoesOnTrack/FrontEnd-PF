ea9b47597cbce2c3a7f26641fef97c231ceeb165
Jessuss <jesusacosta3321@gmail.com>
2023-11-24 15:12:38 -0300

    Estilos SearchBar y Detail

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,15 +1,15 @@
-"use client"
 import React, { useEffect, useState } from 'react';
-import { useDebouncedCallback } from 'use-debounce'
-import lupa from "@/helpers/assets/Lupa.png"
+import { useDebouncedCallback } from 'use-debounce';
+import lupa from "@/helpers/assets/Lupa.png";
 import Image from 'next/image';
+import styles from "./SearchBar.module.css"; // Importa los estilos utilizando estilos modulares
 
 const SearchBar = () => {
     const [name, setName] = useState("");
 
     const handleChange = (event) => {
       setName(event.target.value);
-      handleSearch()
+      handleSearch();
     };
   
     const handleSearch = useDebouncedCallback(() => {
@@ -17,23 +17,25 @@ const SearchBar = () => {
     }, 300);
 
     return (
-        <div>
-            
-      <input
-        type="search"
-        onChange={handleChange}
-        value={name}
-        placeholder="Search for a product..."
-      />
-      <button onClick={()=> setName("")}>
-      <Image
-       src={lupa}
-       width={15}
-       height={15}
-        alt="Search" />
-      </button>
+        <div className={styles.searchbar}>
+            <input
+                type="search"
+                onChange={handleChange}
+                value={name}
+                placeholder="Search for a product..."
+                className={styles.input}
+            />
+            <button onClick={() => setName("")} className={styles.button}>
+                <Image
+                    src={lupa}
+                    className={styles.Lupa}
+                    width={25}
+                    height={25}
+                    alt="Search"
+                />
+            </button>
         </div>
     );
-  };
-  
-  export default SearchBar;
+};
+
+export default SearchBar;
