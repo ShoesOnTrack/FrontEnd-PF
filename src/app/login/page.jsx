0bc1b5c259803701dcd3a620042f197582272d63
SergioVelezH <sergiovelezhernandez11@gmail.com>
2023-11-25 16:59:46 -0300

    Login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,20 +1,33 @@
+import style from "../login/login.module.css";
+import google from "../../helpers/assets/Google.png"
+import Image from 'next/image';
+
 const LoginPage = () => {
-    return (
-        <div>
-            <div>
-                <h3>Iniciar sesión</h3>
-                <label htmlFor="email">Email</label>
-                <input type="email" />
-                <label htmlFor="contraseña">Contraseña</label>
-                <input type="contraseña" name="" id="" />
-                <button>¿Olvidaste tu constraseña?</button>
-                <button>Ingresar</button>
-                <button>Ingresa con tu cuenta de Google</button>
-                <button>¿No tienes cuenta?</button>
-                <button>Crear Usuario</button>
-            </div>
-        </div>
-    );
-  };
-  
-  export default LoginPage;
+  return (
+    <div className={style.pageContainer}>
+      <div className={style.container}>
+        <h3 className={style.iniciar}>  Iniciar sesión  </h3>
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" placeholder="correo@gmail.com" />
+        <label htmlFor="contraseña">Contraseña:</label>
+        <input type="password" placeholder="*******" />
+        <button className={style.olvidaste}>¿Olvidaste tu constraseña?</button>
+        <button className={style.ingresar}>Ingresar</button>
+        <button className={style.terceros}>
+            <Image
+                src={google}
+                width={25}
+                height={25}
+                className={style.google}
+            />
+            <span className={style.span}>Ingresa con tu cuenta de Google</span>
+            </button>
+        <button className={style.olvidaste}>¿No tienes cuenta?</button>
+        <button className={style.crear}>Crear Usuario</button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
