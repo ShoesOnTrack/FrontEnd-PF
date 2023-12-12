e53ead1c83fb18a2f51759606e7a5cfa3ebef5c2
FVidalYepz <Frananyelvidalyepez@gmail.com>
2023-12-12 14:17:36 -0400

    Organización

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,15 @@
-"use client"
-import HomePage from "./home/home"
-import { Provider } from "react-redux"
-import store from "@/redux/store"
+"use client";
+import HomePage from "./home/home";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 const LoginPage = () => {
   return (
-      <div>
-        <Provider store={store}>
-          <HomePage />
+    <div>
+      <Provider store={store}>
+        <HomePage />
       </Provider>
-      </div>
+    </div>
   );
 };
 
