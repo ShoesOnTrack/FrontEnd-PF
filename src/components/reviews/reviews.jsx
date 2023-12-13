46b13cd40f80e6e323292702687ac3217510f516
Agustin Almonacid <agusalmona@gmail.com>
2023-12-13 09:37:07 -0300

    textarea

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -1,61 +1,149 @@
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getTestimonials } from '../../redux/actions.js';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-import { Rating } from 'react-simple-star-rating';
-import style from './style.module.css'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTestimonials } from "../../redux/actions.js";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import Swal from "sweetalert2";
+import { Pagination } from "swiper/modules";
+import { Rating } from "react-simple-star-rating";
+import style from "./style.module.css";
 
 const Reviews = () => {
-	const dispatch = useDispatch();
-	const reviews = useSelector(state => state.reviews)
+  const [rating, setRating] = useState(1);
+  const [message, setMessage] = useState("");
+  const [review, setReview] = useState({});
 
+  const handleRating = (rate) => {
+    setRating(rate);
+  };
 
-	useEffect(() => {
-		dispatch(getTestimonials());
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/reviews", {
+        userId: localStorage.getItem("id"),
+        contenido: message,
+        puntuacion: rating,
+      });
+      if (!data.error) {
+        Swal.fire({
+          title: data.message,
+          icon: "success",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	}, [dispatch]);
+  useEffect(() => {
+    const fetchReview = async () => {
+      try {
+        const { data } = await axios.get(
+          `/reviews/${localStorage.getItem("id")}`
+        );
+        setReview(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchReview();
+  }, []);
 
+  const dispatch = useDispatch();
+  const reviews = useSelector((state) => state.reviews);
 
-	return (
-		<>
-			<div className='container'>
-				<h2 className='text-center'>Reviews</h2>
-				<Swiper
-					slidesPerView={3}
-					spaceBetween={30}
-					modules={[Pagination]}
-					className="mySwiper"
-				>
-					{
-						reviews.data?.map(review => (
-							<SwiperSlide className={style.card}>
-								<h4>{review.usuario?.persona?.nombre} {review.usuario?.persona?.apellido}</h4>
-								<div
-									style={{
-										direction: 'ltr',
-										fontFamily: 'sans-serif',
-										touchAction: 'none'
-									}}
-								>
-									<Rating
-										initialValue={review.puntuacion}
-										onClick={function noRefCheck() { }}
-										readonly
-										allowFraction
-									/>
-								</div>
-								{review.contenido}
-							</SwiperSlide>
-						))
-					}
+  useEffect(() => {
+    dispatch(getTestimonials());
+  }, [dispatch]);
 
-				</Swiper>
-			</div>
-		</>
-	);
-}
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center">Reviews</h2>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {reviews.data?.map((review) => (
+            <SwiperSlide className={style.card}>
+              <h4>
+                {review.usuario?.persona?.nombre}{" "}
+                {review.usuario?.persona?.apellido}
+              </h4>
+              <div
+                style={{
+                  direction: "ltr",
+                  fontFamily: "sans-serif",
+                  touchAction: "none",
+                }}
+              >
+                <Rating
+                  initialValue={reviews.puntuacion}
+                  onClick={function noRefCheck() {}}
+                  readonly
+                  allowFraction
+                />
+              </div>
+              {review.contenido}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className={style.reviews_container}>
+        {review ? (
+          <div className={style.review_edit_container}>
+            <div className={style.review}>
+              <div
+                style={{
+                  direction: "ltr",
+                  fontFamily: "sans-serif",
+                  touchAction: "none",
+                  width: "auto",
+                  display: "inline",
+                }}
+              >
+                <Rating
+                  allowFraction
+                  initialValue={review.puntuacion}
+                  onClick={handleRating}
+                  readonly={true}
+                />
+              </div>
+              <textarea value={review.contenido} disabled={true} />
+            </div>
+          </div>
+        ) : (
+          <div className={style.review_container}>
+            <h3>Danos tu opinion</h3>
+            <div
+              style={{
+                direction: "ltr",
+                fontFamily: "sans-serif",
+                touchAction: "none",
+                textAlign: "center",
+              }}
+            >
+              <Rating allowFraction initialValue={0.5} onClick={handleRating} />
+            </div>
+            <form className={style.form_container} onSubmit={handleSubmit}>
+              <textarea
+                name=""
+                id=""
+                cols="45"
+                rows="8"
+                onChange={(event) => setMessage(event.target.value)}
+              ></textarea>
+              <button type="submit">Crear</button>
+            </form>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Reviews
+export default Reviews;
